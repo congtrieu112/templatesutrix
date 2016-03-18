4bc7fe6fbd1b4b58f7c35a9b516f094da1cfbe21
Dinh Cong Trieu <trieu.dinh@sutrixmedia.com>
2016-03-18 18:10:36 +0700

    sdf

--- modules/custom/block_custom/src/Form/BatchCustom.php
+++ modules/custom/block_custom/src/Form/BatchCustom.php
@@ -1,22 +1,17 @@
 <?php
-
 /**
  * @file
  * Contains Drupal\block_custom\Form\BatchCustom.
  */
-
 namespace Drupal\block_custom\Form;
-
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
-
 /**
  * Class BatchCustom.
  *
  * @package Drupal\block_custom\Form
  */
 class BatchCustom extends ConfigFormBase {
-
   /**
    * {@inheritdoc}
    */
@@ -25,14 +20,12 @@ class BatchCustom extends ConfigFormBase {
         'block_custom.batchcustom',
     ];
   }
-
   /**
    * {@inheritdoc}
    */
   public function getFormId() {
     return 'batch_custom';
   }
-
   /**
    * {@inheritdoc}
    */
@@ -43,36 +36,39 @@ class BatchCustom extends ConfigFormBase {
     foreach ($all_type as $key => $value) {
       $array[$key] = $value->toArray()['name'];
     }
-
     $form['content_type'] = [
         '#type' => 'select',
        '#title' => t('Selected content type'),
        '#options' => $array,
-       '#default_value' => $config->get('content_type'),
+       '#default_value' => "",
        '#description' => t('Set this to <em>Yes</em> if you would like this category to be selected by default.'),
     ];
     $form['number_recore'] = [
         '#type' => 'number',
         '#title' => $this->t('number recore'),
         '#description' => $this->t('input number recore'),
-        '#default_value' => $config->get('number_recore'),
+        '#default_value' => "",
     ];
+      $form['select_option'] = [
+          '#type' => 'select',
+          '#title' => t('Selected option'),
+          '#options' => array(1=>'import data',2=>'export data'),
+          '#default_value' => "",
+          '#description' => t('Set this to <em>Yes</em> if you would like this category to be selected by default.'),
+      ];
     return parent::buildForm($form, $form_state);
   }
-
   /**
    * {@inheritdoc}
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
     parent::validateForm($form, $form_state);
   }
-
   /**
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
 //    parent::submitForm($form, $form_state);
-
 //    $this->config('block_custom.batchcustom')
 //      ->set('content_type', $form_state->getValue('content_type'))
 //      ->set('number_recore', $form_state->getValue('number_recore'))
@@ -80,12 +76,11 @@ class BatchCustom extends ConfigFormBase {
     $batch = array(
       'title' => $this->t('Exporting'),
       'operations' => array(
-        array('disc_migrate', array('courses', array('type'=>$form_state->getValue('content_type'),'number' => $form_state->getValue('number_recore')))),
+        array('disc_migrate', array('courses', array('type'=>$form_state->getValue('content_type'),'number' => $form_state->getValue('number_recore'),'option'=>$form_state->getValue('select_option')))),
       ),
       'finished' => 'disc_migrate_finished_callback',
       'file' => drupal_get_path('module', 'block_custom') . '/disc.migrate.inc',
     );
-
     batch_set($batch);
 //    return batch_process('user');
   }
